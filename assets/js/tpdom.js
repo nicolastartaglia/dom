ed3d63015111a32c84ae2b77f89044cacd47c3f7
nicolastartaglia <ntartaglia@gmail.com>
2021-04-14 16:41:43 +0200

    dom

---
+++ assets/js/tpdom.js
@@ -0,0 +1,7 @@
+function changerStyle(){
+    var x = document.getElementById('parag1');x
+    x.style.color="white";
+    x.style.backgroundColor = "black";
+    x.style.border = "1px dashed rgb(223, 9, 9)";
+    x.style.padding = "5px";
+}
